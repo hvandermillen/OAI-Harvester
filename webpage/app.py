a6b86c0b31fc1dd9182e0b829d2952e054150da4
hvandermillen <hvandermillen@gmail.com>
2025-08-08 17:06:06 -0400

    search term no longer resets on search

--- webpage/app.py
+++ webpage/app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 @app.route("/", methods=['POST','GET'])
 def home():
     if request.method == 'POST':
+        term = request.form.get("term")
+        numResults = request.form.get("numResults")
         records = searchRecords(request.form.get("term"), request.form.get("numResults"))
-        return render_template("searchPage.html", results = records)
-    return render_template("searchPage.html", results = [])
+        return render_template("searchPage.html", results = records, term = term,  numResults = numResults)
+    return render_template("searchPage.html", results = [], term = "business", numResults = "5")
 
 # main driver function
 if __name__ == '__main__':
